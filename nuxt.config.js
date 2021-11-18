9033b445dc2920b0cdc4fa8d3f83f3566b29e365
Matthew Smith <musicsmith4@gmail.com>
2021-11-18 11:24:16 -0500

    setup fonts and global styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,24 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'space-tourism',
+    title: 'Space Tourism',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ['@/assets/css/global.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -33,13 +29,19 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    // https://www.npmjs.com/package/@nuxtjs/google-fonts
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: { Bellefair: true, Barlow: [400], 'Barlow+Condensed': [400] },
+        display: 'swap',
+      },
+    ],
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        darkGray: 'hsl(230, 35%, 7%)',
+        lightPurple: 'hsl(231, 77%, 90%)',
+        white: 'hsl(0, 0%, 100%)',
+      },
+      fontFamily: {
+        bellefair: 'Bellefair',
+        barlow: 'Barlow',
+        barlowCondensed: 'Barlow Condensed',
+      },
+      fontSize: {
+        'mobile-heading-1': ['5rem', '6.25rem'],
+        'heading-1': ['9.375rem', '10.75rem'],
+        'heading-2': ['6.25rem', '7.1875rem'],
+        'heading-3': ['3.5rem', '4rem'],
+        'heading-4': ['2rem', '2.3125rem'],
+        'heading-5': [
+          '1.75rem',
+          { lineHeight: '2.125rem', letterSpacing: '4.725px' },
+        ],
+        'mobile-sub-heading-1': [
+          '1rem',
+          { lineHeight: '19px', letterSpacing: '2.7px' },
+        ],
+        'mobile-base': ['0.9375rem', '1.5625rem'],
+        'sub-heading-1': ['2.125rem', '2rem'],
+        'sub-heading-2': [
+          '0.875rem',
+          { lineHeight: '1.0625rem', letterSpacing: '2.3625px' },
+        ],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      animation: ['motion-safe', 'motion-reduce'],
+    },
+  },
+  plugins: [],
+}
